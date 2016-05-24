2c2c04610321b21585488bdce03d9944792a93e1
Robert Böhnke <robb@robb.is>
2016-05-24 11:55:23 +0200

    Improve memory mapping and add palette

--- NES/PPU+IO.swift
+++ NES/PPU+IO.swift
@@ -2,10 +2,28 @@ import Foundation
 
 extension PPU: IO {
     func read(address: Address) -> UInt8 {
-        return VRAM[Int(address % 0x0800)]
+        let wrappedAddress = address % 0x4000
+
+        switch wrappedAddress {
+        case 0x0000 ..< 0x2000:
+            return mapper.read(wrappedAddress)
+        case 0x3F00 ..< 0x4000:
+            return palette[Int(wrappedAddress % 32)]
+        default:
+            return VRAM[Int(wrappedAddress % 0x0800)]
+        }
     }
 
     func write(address: Address, _ value: UInt8) {
-        VRAM[Int(address % 0x0800)] = value
+        let wrappedAddress = address % 0x4000
+
+        switch wrappedAddress {
+        case 0x0000 ..< 0x2000:
+            mapper.write(wrappedAddress, value)
+        case 0x3F00 ..< 0x4000:
+            palette[Int(wrappedAddress % 32)] = value
+        default:
+            VRAM[Int(wrappedAddress % 0x0800)] = value
+        }
     }
 }
