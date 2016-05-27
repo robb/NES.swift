f4cdf29493542ab751c7cfd36a1e93025f1616e4
Robert Böhnke <robb@robb.is>
2016-05-27 19:16:24 +0200

    Add palette and VRAM mirroring

--- NES/PPU+IO.swift
+++ NES/PPU+IO.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+internal enum MirroringMode: Int {
+    case Horizontal = 0
+    case Vertical = 1
+    case FirstScreen = 2
+    case SecondScreen = 3
+}
+
 extension PPU: IO {
     func read(address: Address) -> UInt8 {
         let wrappedAddress = address % 0x4000
@@ -7,10 +14,16 @@ extension PPU: IO {
         switch wrappedAddress {
         case 0x0000 ..< 0x2000:
             return mapper.read(wrappedAddress)
+        case 0x2000 ..< 0x3F00:
+            let mirrored = mirrorVRAM(wrappedAddress)
+
+            return VRAM[mirrored]
         case 0x3F00 ..< 0x4000:
-            return palette[wrappedAddress % 32]
+            let mirrored = mirrorPalette(wrappedAddress)
+
+            return palette[mirrored]
         default:
-            return VRAM[wrappedAddress % 0x0800]
+            fatalError("Attempt to read illegal PPU memory address \(format(address)).")
         }
     }
 
@@ -20,10 +33,35 @@ extension PPU: IO {
         switch wrappedAddress {
         case 0x0000 ..< 0x2000:
             mapper.write(wrappedAddress, value)
+        case 0x2000 ..< 0x3F00:
+            let mirrored = mirrorVRAM(wrappedAddress)
+
+            VRAM[mirrored] = value
         case 0x3F00 ..< 0x4000:
-            palette[wrappedAddress % 32] = value
+            let mirrored = mirrorPalette(wrappedAddress)
+
+            palette[mirrored] = value
         default:
-            VRAM[wrappedAddress % 0x0800] = value
+            fatalError("Attempt to wirte illegal PPU memory address \(format(address)).")
         }
+    }
+
+    internal func mirrorVRAM(address: Address, mirroringMode: MirroringMode = .Horizontal) -> Address {
+        let lookup: [[UInt16]] = [
+            [0, 0, 1, 1],
+            [0, 1, 0, 1],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1]
+        ]
+
+        let wrappedAddress = (address - 0x2000) % 0x1000
+        let table = wrappedAddress / 0x0400
+        let offset = wrappedAddress % 0x0400
+
+        return  lookup[mirroringMode.rawValue][table] * 0x0400 + offset
+    }
+
+    internal func mirrorPalette(address: Address) -> Address {
+        return address % 32
     }
 }
