8233c5f386698047f35ff0dc6854b4d0336e9556
Robert Böhnke <robb@robb.is>
2019-03-19 16:53:44 +0100

    Flatten P register

--- NES/CPU.swift
+++ NES/CPU.swift
@@ -18,7 +18,69 @@ internal final class CPU {
     /// The P register.
     ///
     /// This register holds the processor flags.
-    var p: UInt8 = 0x24
+    var p: UInt8 {
+        get {
+            var result: UInt8 = 0
+
+            result[bit: 0] = c
+            result[bit: 1] = z
+            result[bit: 2] = i
+            result[bit: 3] = d
+            result[bit: 5] = b
+            result[bit: 6] = v
+            result[bit: 7] = n
+
+            return result
+        }
+        set {
+            c = newValue[bit: 0]
+            z = newValue[bit: 1]
+            i = newValue[bit: 2]
+            d = newValue[bit: 3]
+            b = newValue[bit: 5]
+            v = newValue[bit: 6]
+            n = newValue[bit: 7]
+        }
+    }
+
+    /// Carry.
+    ///
+    /// If `true`, the last addition or shift resulted in a carry or the last
+    /// subtraction resulted in no borrow.
+    var c: Bool = false
+
+    /// Zero.
+    ///
+    /// If `true`, the last operation resulted in `0`.
+    var z: Bool = false
+
+    /// Interrupt inhibit.
+    ///
+    /// If `true`, only non-maskable interrupts can be triggered.
+    var i: Bool = true
+
+    /// Decimal.
+    ///
+    /// If `true`, `ADC` and `SBC` _should_ use binary-coded decimals. However,
+    /// this flag has no effect on the NES and is only present here for sake of
+    /// completeness.
+    var d: Bool = false
+
+    /// Break.
+    ///
+    /// Set by `BRK`.
+    var b: Bool = false
+
+    /// Overflow.
+    ///
+    /// If `true`, the last `ADC` or `SBC` resulted in signed overflow, or the
+    /// 6th bit of the last `BIT` was set.
+    var v: Bool = true
+
+    /// Negative.
+    ///
+    /// If `true`, the last operation resulted in a negative number.
+    var n: Bool = false
 
     /// The A register.
     var a: UInt8 = 0
@@ -51,5 +113,21 @@ internal final class CPU {
     init(mapper: Mapper, ram data: Data = Data(repeating: 0x00, count: 0x800)) {
         self.mapper = mapper
         self.ram = ContiguousArray(data)
+    }
+}
+
+internal extension CPU {
+    /// A convenience method for setting the A register as well as the Zero and
+    /// Negative flags.
+    func updateAZN(_ value: UInt8) {
+        a = value
+        z = value == 0
+        n = value & 0x80 != 0
+    }
+
+    /// A convenience method for setting the Zero and Negative flags.
+    func updateZN(_ value: UInt8) {
+        z = value == 0
+        n = value & 0x80 != 0
     }
 }
